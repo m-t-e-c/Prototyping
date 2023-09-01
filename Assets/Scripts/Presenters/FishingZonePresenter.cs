65cb8bfb92ea917a00a473cb5ddca357290d5168
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-09-02 01:15:14 +0300

    chore: files

---
+++ Assets/Scripts/Presenters/FishingZonePresenter.cs
@@ -0,0 +1,29 @@
+﻿using FishingIdle.Models;
+using FishingIdle.UIElements;
+using UnityEngine;
+
+namespace FishingIdle.Presenters
+{
+    public class FishingZonePresenter : BasePresenter<FishingZonePresenter>
+    {
+        [SerializeField] UIButton startFishingButton;
+
+        FishingZoneModel _model;
+
+        public void Init(string zoneID)
+        {
+            _model = new FishingZoneModel(zoneID);
+            startFishingButton.onClick.AddListener(OnStartFishingButtonClicked);
+        }
+
+        void OnStartFishingButtonClicked()
+        {
+            _model.StartFishing();
+        }
+
+        void OnDestroy()
+        {
+            _model.Dispose();
+        }
+    }
+}
